edeaa0ea8915be44e215d9ca24ee11e85c66e252
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-07-23 12:02:06 +0100

    npm install and add gitignore file

--- client/src/app.js
+++ client/src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 window.addEventListener('load', function () {
   ReactDOM.render(
-    <h1>App Started</h1>,
+    <h1>App Started on Node.js</h1>,
     document.getElementById('app')
   );
 });
